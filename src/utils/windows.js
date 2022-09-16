17bd34aa5cf33668afa34a7d3c391a937ce5fbfd
dragunwf <plarisanmarc@gmail.com>
2022-09-16 23:30:01 +0800

    Implemented main window creation on its own class

--- src/utils/windows.js
+++ src/utils/windows.js
@@ -1,5 +1,21 @@
-const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const { BrowserWindow } = require("electron");
+const GeneralTool = require("./generalTool");
 
-class Windows {}
+class Windows {
+  static #defaultSettings = {
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  };
+  static #mainWindow;
+
+  static createMainMenu() {
+    this.#mainWindow = new BrowserWindow(this.#defaultSettings);
+    this.#mainWindow.loadURL(GeneralTool.getPagePath("index"));
+  }
+}
 
 module.exports = Windows;
