7aec6a2f11611765cd99d957aaa2763098050f45
dragunwf <plarisanmarc@gmail.com>
2022-09-16 22:09:50 +0800

    Implemented method for getting page path in generalTool.js

--- src/utils/generalTool.js
+++ src/utils/generalTool.js
@@ -1,5 +1,10 @@
 const path = require("path");
 
-class GeneralTool {}
+class GeneralTool {
+  static getPagePath(htmlFile) {
+    const file = `${htmlFile}.html`;
+    return `file:///${path.join(__dirname, "..", "pages/html/", file)}`;
+  }
+}
 
 module.exports = GeneralTool;
