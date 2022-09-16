a6ce0b981d382452c1109c58ab0020b994b0346b
dragunwf <plarisanmarc@gmail.com>
2022-09-16 23:45:15 +0800

    Cleaned up code and implemented method for creating window to source code

--- src/utils/windows.js
+++ src/utils/windows.js
@@ -12,15 +12,25 @@ class Windows {
   };
   static #mainWindow;
   static #rickRollWindow;
+  static #sourceCodeWindow;
 
   static createMainMenu() {
-    this.#mainWindow = new BrowserWindow(this.#defaultSettings);
+    this.#mainWindow = this.#createDefaultWindow();
     this.#mainWindow.loadURL(GeneralTool.getPagePath("index"));
   }
 
   static createRickRollWindow() {
-    this.#rickRollWindow = new BrowserWindow(this.#defaultSettings);
+    this.#rickRollWindow = this.#createDefaultWindow();
     this.#rickRollWindow.loadURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  }
+
+  static createSourceCodeWindow() {
+    this.#sourceCodeWindow = this.#createDefaultWindow();
+    this.#sourceCodeWindow.loadURL("https://github.com/DragunWF/QuickNotes");
+  }
+
+  static #createDefaultWindow() {
+    return new BrowserWindow(this.#defaultSettings);
   }
 }
 
