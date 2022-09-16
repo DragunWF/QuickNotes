8ad274bdf60d8532e297d4a5f9b94791fc69ca44
dragunwf <plarisanmarc@gmail.com>
2022-09-16 23:59:04 +0800

    Implemented frontend page switching

--- src/pages/js/main.js
+++ src/pages/js/main.js
@@ -1,6 +1,23 @@
 const { ipcRenderer } = require("electron");
 window.$ = window.jQuery = require("../js/jquery-3.6.0.min.js");
 
-$("#quitBtn").click(() => {
+const htmlDirPath = "file:///E:/DevStuff/GitHub Repositories/QuickNotes/src/pages/html/";
+const notesBtn = $("#notesBtn");
+const statsBtn = $("#statsBtn");
+const quitBtn = $("#quitBtn");
+
+notesBtn.click(() => {
+  switchPageURL("categories");
+});
+
+statsBtn.click(() => {
+  switchPageURL("stats");
+});
+
+quitBtn.click(() => {
   ipcRenderer.send("quit");
 });
+
+function switchPageURL(htmlFile) {
+  location.href = `${htmlDirPath}${htmlFile}.html`;
+}
