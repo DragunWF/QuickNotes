cdf92edc16f967b53eaf57fb8dcb4e800ce69de4
dragunwf <plarisanmarc@gmail.com>
2022-09-25 17:20:29 +0800

    Called onReady() method for dbTool in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const { app, Menu, ipcMain } = require("electron");
 const GeneralTool = require("./utils/generalTool");
+const DatabaseTool = require("./utils/dbTool");
 const Windows = require("./utils/windows");
 const menuTemplate = require("./utils/menuTemplate");
 
 app.on("ready", () => {
   Windows.createMainMenu();
+  DatabaseTool.onReady();
   const windowMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(windowMenu);
 });
