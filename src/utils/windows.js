eb4b36952bda49a0d7794892d663032d1371e59b
dragunwf <plarisanmarc@gmail.com>
2022-09-27 01:15:09 +0800

    Implemented window loading for a category

--- src/utils/windows.js
+++ src/utils/windows.js
@@ -34,11 +34,15 @@ class Windows {
     this.#changeCurrentWindow("categories", "load:categories");
   }
 
-  static loadNoteWindow() {
-    this.#changeCurrentWindow("note", "load:note");
+  static loadCategoryWindow(categoryName) {
+    this.#changeCurrentWindow("category", "load:category", categoryName);
   }
 
-  static async #changeCurrentWindow(page, loadSignal = null, noteID = null) {
+  static loadNoteWindow(noteID) {
+    this.#changeCurrentWindow("note", "load:note", noteID);
+  }
+
+  static async #changeCurrentWindow(page, loadSignal = null, id = null) {
     const currentWindow = BrowserWindow.getFocusedWindow();
     let data = null;
     currentWindow.loadURL(GeneralTool.getPagePath(page));
@@ -46,7 +50,10 @@ class Windows {
       const signalType = loadSignal.split(":")[1];
       switch (signalType) {
         case "note":
-          data = DatabaseTool.getNote(noteID);
+          data = DatabaseTool.getNote("note_id", id);
+          break;
+        case "category":
+          data = DatabaseTool.getCategory("name", id);
           break;
         case "categories":
           data = DatabaseTool.getNoteCategories();
