92e6d674019c75533a51dbd094aa55a9740d0d26
dragunwf <plarisanmarc@gmail.com>
2022-09-27 00:17:22 +0800

    Added more event handlers for switching pages and implemented logging for app.js

--- src/app.js
+++ src/app.js
@@ -4,16 +4,30 @@ const DatabaseTool = require("./utils/dbTool");
 const Windows = require("./utils/windows");
 const menuTemplate = require("./utils/menuTemplate");
 
+// App Events Handlers
 app.on("ready", () => {
   Windows.createMainMenu();
   DatabaseTool.onReady();
+
   const windowMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(windowMenu);
+
   console.log("Application is now running!");
 });
 
 app.on("window-all-closed", () => {
   app.quit();
+});
+
+// ipcMain Events Handlers
+ipcMain.on("window:categories", () => {
+  Windows.loadCategoriesWindow();
+  console.log("Entered categories window!");
+});
+
+ipcMain.on("window:note", () => {
+  Windows.loadNoteWindow();
+  console.log("Entered a note window!");
 });
 
 ipcMain.on("quit", () => {
