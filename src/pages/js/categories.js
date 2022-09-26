06a34ef12b15eac5198adfb0e2c75fca401ff3b7
dragunwf <plarisanmarc@gmail.com>
2022-09-27 00:52:09 +0800

    Implemented event handling for category buttons

--- src/pages/js/categories.js
+++ src/pages/js/categories.js
@@ -2,7 +2,13 @@ const { ipcRenderer } = require("electron");
 window.$ = window.jQuery = require("../js/jquery-3.6.0.min.js");
 
 const categories = $(".category-list");
-const category = $(".category");
+
+function createEvents() {
+  $(".category").click((event) => {
+    const id = event.target.id;
+    console.log(`Entered note with id: ${id}`);
+  });
+}
 
 ipcRenderer.on("load:categories", async (event, data) => {
   const categoryData = await data;
@@ -10,11 +16,14 @@ ipcRenderer.on("load:categories", async (event, data) => {
   if (categoryData.length) {
     for (let i = 0; i < categoryData.length; i++) {
       categories.append(
-        `<li class=".category" id="category@${i + 1}">
-           <button>${categoryData[i].name}</button>
+        `<li>
+           <button type="button" class="category" id="item@${i + 1}">
+             ${categoryData[i].name}
+           </button>
          </li>`
       );
     }
+    createEvents();
   } else {
     const tag = "<li>No categories, click add to make one</li>";
     categories.append(tag);
