f657a9d0bc2b765482da2239b7514a03caaa2e51
dragunwf <plarisanmarc@gmail.com>
2022-09-27 00:09:32 +0800

    Fixed categories implementation

--- src/pages/js/categories.js
+++ src/pages/js/categories.js
@@ -2,12 +2,17 @@ const { ipcRenderer } = require("electron");
 window.$ = window.jQuery = require("../js/jquery-3.6.0.min.js");
 
 const categories = $(".category-list");
+const category = $(".category");
 
-ipcRenderer.on("load:categories", (event, data) => {
-  if (data.length) {
-    for (let i = 0; i < data.length; i++) {
+ipcRenderer.on("load:categories", async (event, data) => {
+  const categoryData = await data;
+  $("#loadingText").remove();
+  if (categoryData.length) {
+    for (let i = 0; i < categoryData.length; i++) {
       categories.append(
-        `<li class=".category-button" id="category${i}"><button>${data.name}</button></li>`
+        `<li class=".category" id="category@${i + 1}">
+           <button>${categoryData[i].name}</button>
+         </li>`
       );
     }
   } else {
